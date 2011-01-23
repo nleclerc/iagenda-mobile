1e8a5697bb4509696919e4d6d40123614316fdf9
Nicolas Leclerc <nl@spirotron.fr>
2011-01-23 01:02:44 +0100

    Added login form links to github project and current url qrcode.

--- login.php
+++ login.php
@@ -36,6 +36,25 @@ if ($receivedLogin) {
 <link rel="shortcut icon" href="images/favicon.png">
 <link rel="stylesheet" href="css/main.css" type="text/css" />
 
+<script type="text/javascript" src="scripts/jquery-1.4.4.min.js"></script>
+<script type="text/javascript">
+
+$(init)
+
+function init(){
+	$('#qrcodelink').click(toggleQRCode);
+}
+
+function toggleQRCode(){
+	var code = $('#qrcode');
+	if (code.html() == '')
+		code.html('<img class="qrcode" src="http://chart.apis.google.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=chld=L|1&chl='+window.location.href+'">');
+	else
+		code.html('');
+}
+
+</script>
+
 <script type="text/javascript" src="/ga.js"></script>
 </head>
 <body>
@@ -57,6 +76,11 @@ if ($receivedLogin) {
 <input type="password" name="pwd" value="<?=$receivedPassword?>">
 
 <input id="loginSubmit" type="submit" value="Valider">
+
+<a class="footerLink" href="https://github.com/nleclerc/iagenda-mobile">Code Source</a>
+<a class="footerLink" id="qrcodelink" href="#">QRCode</a>
+
+<div id="qrcode"></div>
 </div>
 </form>
 
