acc79f9a7e53405c7fde0fad281be8f1fd28b8b6
Nicolas Leclerc <nl@spirotron.fr>
2011-02-01 23:27:36 +0100

    Moved all login form html to js logic.

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,36 @@
 
+function setContentBody (html){
+	$('#contentBody').hide().html(html).fadeIn(200);
+}
+
+function showLoginForm(){
+	
+	var body = '';
+	
+	// The iframe and form action are used to allow browsers to save login information.
+	body += '<iframe name="dummyFrame" class="hidden" src="dummy.txt"></iframe>';
+	body += '<form name="loginForm" id="loginForm" target="dummyFrame" action="dummy.txt">';
+	body += '<div id="errorMessage"></div>';
+	body += '<div class="inputLabel">Identifiant</div>';
+	body += '<input type="text" id="login" name="login">';
+
+	body += '<div class="inputLabel">Mot de passe</div>';
+	body += '<input type="password" id="pwd" name="pwd">';
+
+	body += '<input id="loginSubmit" type="submit" value="Valider">';
+
+	body += '<a class="footerLink" href="mailto:nl@spirotron.fr?subject=[iAgenda] Remarque">Contact</a>';
+	body += '<a class="footerLink" href="https://github.com/nleclerc/iagenda-mobile">Code Source</a>';
+	body += '<a class="footerLink" id="qrcodelink" href="#" onclick="toggleQRCode()">QRCode</a>';
+
+	body += '<div id="qrcode"></div>';
+	body += '</form>';
+		
+	setContentBody(body);
+	
+	$('#loginForm').submit(handleLoginSubmit);
+}
+
 function handleLoginSubmit(eventObject){
 	$.getJSON("services/login.php?login="+$('#login').val()+"&pwd="+$('#pwd').val(), null, handleLoginresult);
 }
